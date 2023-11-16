821f336ed8821e9ab0bd7e7cc29357779a745898
Asox7 <mommensylann@gmail.com>
2023-11-16 18:11:52 +0100

    feat: add homepage logo link functionality

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import { Navbar as BootstrapNavbar } from 'bootstrap';
 
+import Navigate from '../Router/Navigate';
+
 import logoImage from '../../img/icon.png';
 
 const Navbar = () => {
   const navbar = `
     <div class="d-flex justify-content-between align-items-center mb-3 mb-md-0 px-4 py-3" data-uri="/">
-      <a href="/">
+      <a id="homepage-logo">
         <img src="${logoImage}" width="180">
       </a>
       <div class="dropdown show">
@@ -23,6 +25,10 @@ const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   navbarWrapper.innerHTML = navbar;
 
+  const logo = document.querySelector('#homepage-logo');
+  logo.addEventListener('click', () => {
+    Navigate('/');
+  });
 };
 
 export default Navbar;
