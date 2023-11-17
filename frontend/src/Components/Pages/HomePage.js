e6d3ac3998f2b9613fdb66aa5ca8289b4793cc4f
elisenium <elise.grelaud@student.vinci.be>
2023-11-17 11:37:00 +0100

    edit homepage

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -6,24 +6,25 @@ import Navigate from '../Router/Navigate';
 const HomePage = () => {
     const homePage = `
     <section id="homePage">
-      <div class="row ">
-        <div class="homePage-container col-lg-6">
-            <img id="bg" src="${illustrationImage1}" alt="homePage" >
-            
-          </div>
-        <div id ="homePage-info" class="col-lg-6">
-          <h1 class="display-5 fw-bold lh-1 mb-3">
-            Build Your Personalized Skincare Routine
-          </h1>
-          <p class="lead">
-            Acquire a uniquely designed daily plan by easily participating in a brief 2-minute survey. 
-            Our team of professionals will thoroughly evaluate your concerns and devise the perfect regimen specifically for you.              
-          </p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" class="btn btn-lg">Start Quizz</button>
-          </div>
+      <div class="grid-container-homepage">
+        <div class="grid-item-homepage">
+            <img id="bg" src="${illustrationImage1}" alt="homepage" class="homepage">
         </div>
-      </div> 
+        <div class="grid-item-homepage white-background">
+            <div id ="homePage-info">
+              <h1 class="display-5 fw-bold lh-1 mb-3">
+                Build Your Personalized Skincare Routine
+              </h1>
+              <p class="lead">
+                Acquire a uniquely designed daily plan by easily participating in a brief 2-minute survey. 
+                Our team of professionals will thoroughly evaluate your concerns and devise the perfect regimen specifically for you.              
+              </p>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-lg">Start Quizz</button>
+              </div>
+            </div>
+        </div>
+    </div>
     </section>`;
 
   const main = document.querySelector('main');
