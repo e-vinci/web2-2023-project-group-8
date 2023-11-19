94a8e1a88eccebd960d29885f4f62ad8d3ea3428
Asox7 <mommensylann@gmail.com>
2023-11-19 18:45:50 +0100

    feat: update Navbar component with dropdown menu

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -7,16 +7,19 @@ import logoImage from '../../img/icon.png';
 
 const Navbar = () => {
   const navbar = `
-    <div class="d-flex justify-content-between align-items-center mb-3 mb-md-0 px-4 py-3" data-uri="/">
+    <div class="d-flex justify-content-between align-items-center mb-3 mb-md-0 px-4 py-3">
       <a id="homepage-logo">
         <img src="${logoImage}" width="180">
       </a>
       <div class="dropdown show">
-  
-        <div class="hamburger-icon">
-          <span></span>
-          <span></span>
-          <span></span>
+        <div class="hamburger-icon" data-bs-toggle="dropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </div>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" id="dropdown-home">Accueil</a>
+          <a class="dropdown-item" id="dropdown-quiz">Quizz</a>
         </div>
       </div>
     </div>
@@ -25,10 +28,18 @@ const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   navbarWrapper.innerHTML = navbar;
 
-  const logo = document.querySelector('#homepage-logo');
-  logo.addEventListener('click', () => {
+  document.querySelector('#homepage-logo').addEventListener('click', () => {
     Navigate('/');
   });
+
+  document.querySelector('#dropdown-home').addEventListener('click', () => {
+    Navigate('/');
+  });
+
+  document.querySelector('#dropdown-quiz').addEventListener('click', () => {
+    Navigate('/quiz');
+  });
+
 };
 
 export default Navbar;
