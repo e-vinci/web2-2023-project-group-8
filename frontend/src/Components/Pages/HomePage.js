6471608666a6f2485f5db37e69c25587b4ee26e4
Asox7 <mommensylann@gmail.com>
2023-11-19 18:02:38 +0100

    fix: homepage layout and prevent default scrolling

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -5,26 +5,24 @@ import Navigate from '../Router/Navigate';
 
 const HomePage = () => {
     const homePage = `
-    <section id="homePage">
-      <div class="grid-container-homepage">
-        <div class="grid-item-homepage">
-            <img id="bg" src="${illustrationImage1}" alt="homepage" class="homepage">
-        </div>
-        <div class="grid-item-homepage white-background">
-            <div id ="homePage-info">
-              <h1 class="display-5 fw-bold lh-1 mb-3">
-                Build Your Personalized Skincare Routine
-              </h1>
-              <p class="lead">
-                Acquire a uniquely designed daily plan by easily participating in a brief 2-minute survey. 
-                Our team of professionals will thoroughly evaluate your concerns and devise the perfect regimen specifically for you.              
-              </p>
-              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <button type="button" class="btn btn-lg">Start Quizz</button>
-              </div>
+    <section class="grid-container-homepage"">
+      <div class="grid-item-homepage">
+          <img id="bg" src="${illustrationImage1}" alt="homepage" class="homepage">
+      </div>
+      <div class="grid-item-homepage white-background">
+          <div id ="homePage-info">
+            <h1 class="display-5 fw-bold lh-1 mb-3">
+              Build Your Personalized Skincare Routine
+            </h1>
+            <p class="lead">
+              Acquire a uniquely designed daily plan by easily participating in a brief 2-minute survey. 
+              Our team of professionals will thoroughly evaluate your concerns and devise the perfect regimen specifically for you.              
+            </p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" class="btn btn-lg">Start Quizz</button>
             </div>
-        </div>
-    </div>
+          </div>
+      </div>
     </section>`;
 
   const main = document.querySelector('main');
@@ -39,6 +37,7 @@ const HomePage = () => {
   });  
 };
 
+changeBg();
 setInterval(changeBg, 3000);
 
 function changeBg(){
@@ -49,6 +48,16 @@ function changeBg(){
     const bg = images[Math.floor(Math.random() * images.length)];
     img.src = bg;
   }
+}
+
+const body = document.querySelector('body');
+// Ajouter un gestionnaire d'événements sur le body pour empêcher le défilement par défaut
+body.addEventListener('wheel', preventDefaultScroll, { passive: false });
+body.addEventListener('touchmove', preventDefaultScroll, { passive: false });
+
+// Empêcher le défilement par défaut
+function preventDefaultScroll(event) {
+  event.preventDefault();
 }
 
 export default HomePage;
