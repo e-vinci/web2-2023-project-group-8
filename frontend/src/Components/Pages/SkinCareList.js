140b7fde4457d831f3ed2308d34fa007fc98a26a
Asox7 <mommensylann@gmail.com>
2023-11-21 15:15:54 +0100

    refactor(frontend): SkinCareList to fetch skin care data based on user ID

--- frontend/src/Components/Pages/SkinCareList.js
+++ frontend/src/Components/Pages/SkinCareList.js
@@ -1,10 +1,19 @@
+const main = document.querySelector('main');
+
 const Userlist = () => {
-    listSkinCare ();
+    const url = new URL(window.location.href);
+    const userId = url.searchParams.get('userId');
+
+    listSkinCare(userId);
 };
 
-function listSkinCare(){
-    fetch(`http://localhost:3000/users/skinCare`)
-    .then((response) => response.json())
+async function listSkinCare(userId){
+    const response = await fetch(`http://localhost:3000/users/skinCare?userId=${userId}`);
+    const data = await response.json();
+
+    data.forEach((skinCare) => {
+        main.innerHTML += `${skinCare.intitule}`;
+    });
 }
 
 export default Userlist;
