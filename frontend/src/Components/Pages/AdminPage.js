e3ac450ac3b4a6598de4c28d2654e70a4a06cada
Asox7 <mommensylann@gmail.com>
2023-11-21 13:57:39 +0100

    refactor(frontend): simplify promise

--- frontend/src/Components/Pages/AdminPage.js
+++ frontend/src/Components/Pages/AdminPage.js
@@ -14,54 +14,53 @@ const AdminPage = async () => {
     listAllUser();
 };
 
-function listAllUser() {
-    fetch(`http://localhost:3000/users`)
-    .then((response) => response.json())
-    .then((data) => {
-        main.innerHTML  += `
-        <table class="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-              <th>Username</th>
-              <th>Nom</th>
-              <th>Prenom</th>
-              <th>Inscription</th>
-              <th>Derniere connexion</th>
-              <th>Droit</th>
-              <th>Nombre de Routine</th>
-            </tr>
-          </thead>
-          <tbody id="table-body">
-          </tbody>
-        </table>`;
-        
-        const tbody = document.querySelector('#table-body');
-        
-        data.forEach((user) => {
-            tbody.innerHTML += `
-            <tr>
-                <td> <input type="checkbox" class="table-row"></td>
-                <td>
-                    <img src="${user.imageUrl}/${user.id}/${user.photo_profil}" alt="user" class="avatar">
-                </td>
-                <td id="user-info">@${user.username}</td>
-                <td>${user.nom}</td>
-                <td>${user.prenom}</td>
-                <td>${formatFrenchDate(user.created)}</td>
-                <td>${formatFrenchDate(user.derniere_connexion)}</td>
-                <td>${user.verified ? 'Administrateur' : 'Utilisateur'}</td>
-                <td></td>
-            </tr>
-            `;
-        });
-        const username = document.querySelector('#user-info');
-        username.addEventListener('click', () => {
-            
-            Navigate('/user/SkinCareList');
-        }); 
+async function listAllUser() {
+    const response = await fetch(`http://localhost:3000/users`);
+    const data = await response.json();
+
+    main.innerHTML  += `
+    <table class="table">
+        <thead>
+        <tr>
+            <th></th>
+            <th></th>
+            <th>Username</th>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Inscription</th>
+            <th>Derniere connexion</th>
+            <th>Droit</th>
+            <th>Nombre de Routine</th>
+        </tr>
+        </thead>
+        <tbody id="table-body">
+        </tbody>
+    </table>`;
+    
+    const tbody = document.querySelector('#table-body');
+    
+    data.forEach((user) => {
+        tbody.innerHTML += `
+        <tr>
+            <td> <input type="checkbox" class="table-row"></td>
+            <td>
+                <img src="${user.imageUrl}/${user.id}/${user.photo_profil}" alt="user" class="avatar">
+            </td>
+            <td id="user-info">@${user.username}</td>
+            <td>${user.nom}</td>
+            <td>${user.prenom}</td>
+            <td>${formatFrenchDate(user.created)}</td>
+            <td>${formatFrenchDate(user.derniere_connexion)}</td>
+            <td>${user.verified ? 'Administrateur' : 'Utilisateur'}</td>
+            <td></td>
+        </tr>
+        `;
     });
+    const username = document.querySelector('#user-info');
+    username.addEventListener('click', () => {
+        
+        Navigate('/user/SkinCareList');
+    }); 
 }
 
 export default AdminPage;
