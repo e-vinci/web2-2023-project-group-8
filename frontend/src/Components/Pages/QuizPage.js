503f70e25c32714e35903db9ecab3d17efb17567
hadje <hadjera.emroska@student.vinci.be>
2023-11-27 15:26:47 +0100

    update: frontend quizzpage

--- frontend/src/Components/Pages/QuizPage.js
+++ frontend/src/Components/Pages/QuizPage.js
@@ -1,5 +1,4 @@
-import arrowLeft from '../../img/arrow-left.svg';
-import arrowRight from '../../img/arrow-right.svg';
+// import arrowLeft from '../../img/arrow-left.svg';
 
 const QuizPage = () => {
   const quizPage = `
@@ -7,23 +6,18 @@ const QuizPage = () => {
       
       <div class="container">
         <div class="row">
-          <div class="col-2 back-section">
-            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
-            Back
-          </div>
+          
           <div class="col">
             <div class="quiz_question text-center">
-              <h3>Which of the below best describes your skin type, Patrick?</h3>
+              <h3>Quel est votre type de peau ?</h3>
+              <p>Tips : si vous ne connaissez pas votre type de peau vous pouvez vous nettoyer le visage, attendre 30min et si votre peau tiraille: vous avez la peau séche, si votre peau devient luisante sur votre zone t et seche sur vos joues vous avez la peau mixte, si votre peau devient luisante sur l’ensemble vous avez la peau grasse et si votre peau reste douce c’est que vous avez une peau normal</p>
             </div>
-            <div class="d-flex justify-content-center quiz_responses">
-              <button type="button" class="btn btn-lg">Dry</button>
-              <button type="button" class="btn btn-lg">Normal</button>
-              <button type="button" class="btn btn-lg">Oily</button>
+            <div class="d-flex justify-content-center quiz_responses selector1">
+              <button type="button" class="btn btn-lg">peau sèche</button>
+              <button type="button" class="btn btn-lg">peau normale</button>
+              <button type="button" class="btn btn-lg">peau mixte</button>
+              <button type="button" class="btn btn-lg">peau grasse</button>
             </div>
-          </div>
-          <div class="col-2 next-section">
-            Next
-            <img src="${arrowRight}" id="arrow-right" alt="Right arrow">
           </div>
         </div>
       </div>
@@ -31,8 +25,41 @@ const QuizPage = () => {
   `;
   const main = document.querySelector('main');
   main.innerHTML = quizPage;
+
+  const button = document.querySelector('.selector1');
+  button.addEventListener('click', () => {
+    main.innerHTML=`<section id="quiz">
+      
+      <div class="container">
+        <div class="row">
+          
+          <div class="col">
+            <div class="quiz_question text-center">
+              <h3>Quel problème souhaitez-vous traiter en premier ?</h3>
+            </div>
+            <br>
+            <div class="d-flex justify-content-center quiz_responses selector">
+              <button type="button" class="btn btn-lg">boutons</button>
+              <button type="button" class="btn btn-lg">points noirs</button>
+              <button type="button" class="btn btn-lg">rougeurs</button>
+              <button type="button" class="btn btn-lg">rides</button>
+              <button type="button" class="btn btn-lg">teint terne</button>
+              <button type="button" class="btn btn-lg">peau irrégulière</button>
+              <button type="button" class="btn btn-lg">cernes</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>`
+    
   
 
+
+  })
+  
+  
+/*
   const previousButton = document.querySelector('.back-section');
   const quizQuestion = document.querySelector('.quiz_question');
 
@@ -41,7 +68,7 @@ const QuizPage = () => {
     quizQuestion.style.transform = 'translateX(-1000%)';
   }); 
   
-  /*
+  
   const nextButton = document.querySelector('next-section');
   nextButton.addEventListener('click', () => {
     // On va a la question suivante
