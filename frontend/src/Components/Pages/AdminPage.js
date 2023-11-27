f6ec01dd7c6970befb62a5dd1be072dcc48321e8
Asox7 <mommensylann@gmail.com>
2023-11-27 22:14:29 +0100

    Refactor AdminPage.js and main.css

--- frontend/src/Components/Pages/AdminPage.js
+++ frontend/src/Components/Pages/AdminPage.js
@@ -19,49 +19,70 @@ async function listAllUser() {
     const data = await response.json();
 
     main.innerHTML  += `
-    <table class="table">
-        <thead>
-        <tr>
-            <th></th>
-            <th></th>
-            <th>Username</th>
-            <th>Nom</th>
-            <th>Prenom</th>
-            <th>Inscription</th>
-            <th>Derniere connexion</th>
-            <th>Droit</th>
-            <th>Nombre de Routine</th>
-        </tr>
-        </thead>
-        <tbody id="table-body">
-        </tbody>
-    </table>`;
-    
+    <div class="content">
+        <div class="container">
+            <div class="table-responsive custom-table-responsive">
+                <table class="custom-table">
+                    <thead>
+                        <tr>
+                            <th scope="col">
+                                <label class="control control--checkbox">
+                                <input type="checkbox"/>
+                                <div class="control__indicator"></div>
+                                </label>
+                            </th>
+                            <th scope="col">photo de profil</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Prenom</th>
+                            <th scope="col">email</th>
+                            <th scope="col">Derniere connexion</th>
+                            <th scope="col">Accès</th>
+                            <th scope="col">SkinCare</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>`;
+
     const tbody = document.querySelector('#table-body');
-    
+
     data.forEach((user) => {
         tbody.innerHTML += `
-        <tr>
-            <td> <input type="checkbox" class="table-row"></td>
-            <td>
+        <tr class="table-response test">
+            <td class="td-spacing" scope="row">
+                <input type="checkbox"/>
+            </td>
+            <td class="td-spacing" style="text-align: center;">
                 <img src="${user.profileUrl}" alt="user" class="avatar">
             </td>
-            <td id="user-info" data-user-id="${user.id}">@${user.username}</td>
-            <td>${user.nom}</td>
-            <td>${user.prenom}</td>
-            <td>${formatFrenchDate(user.created)}</td>
-            <td>${formatFrenchDate(user.derniere_connexion)}</td>
-            <td>${user.verified ? 'Administrateur' : 'Utilisateur'}</td>
-            <td></td>
+            <td class="td-spacing">@${user.username}</td>
+            <td class="td-spacing">${user.nom}</td>
+            <td class="td-spacing">${user.prenom}</td>
+            <td class="td-spacing">
+                ${user.email}
+                <small class="d-block">Date de création ${formatFrenchDate(user.created)}</small>
+            </td>
+            <td class="td-spacing">${formatFrenchDate(user.derniere_connexion)}</td>
+            <td class="td-spacing">${user.verified ? 'Administrateur' : 'Utilisateur'}</td>
+            <td class="td-spacing">
+                <button type="button" class="btn btn-lg" data-user-id="${user.id}">voir</button>
+            </td>
+            
         </tr>
         `;
     });
-    
-    const username = document.querySelector('#user-info');
-    username.addEventListener('click', (event) => {
-        const {userId} = event.target.dataset;
-        Navigate(`/user/SkinCareList?userId=${userId}`);
-    }); 
+
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            const userId = event.target.getAttribute('data-user-id');
+            Navigate(`/user/SkinCareList?userId=${userId}`);
+        });
+    });
 }
 
 export default AdminPage;
