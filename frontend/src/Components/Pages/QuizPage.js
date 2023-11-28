c96803d48547c8915816a67eb8810ed463831820
hadje <hadjera.emroska@student.vinci.be>
2023-11-28 10:06:28 +0100

    update: add other questions

--- frontend/src/Components/Pages/QuizPage.js
+++ frontend/src/Components/Pages/QuizPage.js
@@ -6,12 +6,11 @@ const QuizPage = () => {
       
       <div class="container">
         <div class="row">
-        <div class="col-2 back-section">
-            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
-            Back
-          </div>
+        <div class="col-2">
+        
+      </div>
           
-          <div class="col">
+          <div class="col-8">
             <div class="quiz_question text-center">
               <h3>Quel est votre type de peau ?</h3>
               <p>Tips : si vous ne connaissez pas votre type de peau vous pouvez vous nettoyer le visage, attendre 30min et si votre peau tiraille: vous avez la peau séche, si votre peau devient luisante sur votre zone t et seche sur vos joues vous avez la peau mixte, si votre peau devient luisante sur l’ensemble vous avez la peau grasse et si votre peau reste douce c’est que vous avez une peau normal</p>
@@ -41,7 +40,7 @@ const QuizPage = () => {
             Back
           </div>
           
-          <div class="col">
+          <div class="col-8">
             <div class="quiz_question text-center">
               <h3>Quel problème souhaitez-vous traiter en premier ?</h3>
             </div>
@@ -60,18 +59,165 @@ const QuizPage = () => {
         </div>
       </div>
     </section>`;
+
+    const button2 = document.querySelector('.selector2');
+    button2.addEventListener('click', () => {
+      main.innerHTML = `<section id="quiz">
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-2 back-section">
+            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
+            Back
+          </div>
+          
+          <div class="col-8">
+            <div class="quiz_question text-center">
+              <h3>Quel est votre budget ?</h3>
+            </div>
+            <br>
+            <div class="d-flex justify-content-center quiz_responses selector3">
+              <button type="button" class="btn btn-lg">petit budget</button>
+              <button type="button" class="btn btn-lg">budget moyen</button>
+              <button type="button" class="btn btn-lg">gros budget</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>`;
+
+      const button3 = document.querySelector('.selector3');
+      button3.addEventListener('click', () => {
+        main.innerHTML = `<section id="quiz">
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-2 back-section">
+            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
+            Back
+          </div>
+          
+          <div class="col-8">
+            <div class="quiz_question text-center">
+              <h3>Dans quelle tranche d'âge est-ce que vous vous situez ?</h3>
+            </div>
+            <br>
+            <div class="d-flex justify-content-center quiz_responses selector4">
+              <button type="button" class="btn btn-lg">moins de 18 ans</button>
+              <button type="button" class="btn btn-lg">entre 18 et 25 ans</button>
+              <button type="button" class="btn btn-lg">entre 26 et 35 ans</button>
+              <button type="button" class="btn btn-lg">entre 36 et 50 ans</button>
+              <button type="button" class="btn btn-lg">plus de 50 ans</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>`;
+
+        const button4 = document.querySelector('.selector4');
+        button4.addEventListener('click', () => {
+          main.innerHTML = `<section id="quiz">
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-2 back-section">
+            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
+            Back
+          </div>
+          
+          <div class="col-8">
+            <div class="quiz_question text-center">
+              <h3>À quelle fréquence êtes-vous exposé au soleil ?</h3>
+            </div>
+            <br>
+            <div class="d-flex justify-content-center quiz_responses selector5">
+              <button type="button" class="btn btn-lg">jamais</button>
+              <button type="button" class="btn btn-lg">rarement</button>
+              <button type="button" class="btn btn-lg">parfois</button>
+              <button type="button" class="btn btn-lg">souvent</button>
+              <button type="button" class="btn btn-lg">tout le temps</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>`;
+
+          const button5 = document.querySelector('.selector5');
+          button5.addEventListener('click', () => {
+            main.innerHTML = `<section id="quiz">
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-2 back-section">
+            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
+            Back
+          </div>
+          
+          <div class="col-8">
+            <div class="quiz_question text-center">
+              <h3>Portez-vous du maquillage? Si oui, à quelle fréquence ?</h3>
+            </div>
+            <br>
+            <div class="d-flex justify-content-center quiz_responses selector6">
+              <button type="button" class="btn btn-lg">jamais</button>
+              <button type="button" class="btn btn-lg">pour des occasions particulières</button>
+              <button type="button" class="btn btn-lg">souvent</button>
+              <button type="button" class="btn btn-lg">tous les jours</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>`;
+            const button6 = document.querySelector('.selector6');
+            button6.addEventListener('click', () => {
+              main.innerHTML = `<section id="quiz">
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-2 back-section">
+            <img src="${arrowLeft}" id="arrow-left" alt="Left arrow">
+            Back
+          </div>
+          
+          <div class="col-8">
+            <div class="quiz_question text-center">
+              <h3>Souhaitez vous traiter un autre problème ?</h3>
+            </div>
+            <br>
+            <div class="d-flex justify-content-center quiz_responses selector7">
+            <button type="button" class="btn btn-lg">boutons</button>
+            <button type="button" class="btn btn-lg">points noirs</button>
+            <button type="button" class="btn btn-lg">rougeurs</button>
+            <button type="button" class="btn btn-lg">rides</button>
+            <button type="button" class="btn btn-lg">teint terne</button>
+            <button type="button" class="btn btn-lg">peau irrégulière</button>
+            <button type="button" class="btn btn-lg">cernes</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>`;
+            });
+          });
+        });
+      });
+    });
+
+    const previousButton = document.querySelector('.back-section');
+    const quizQuestion = document.querySelector('.quiz_question');
+
+    previousButton.addEventListener('click', () => {
+      quizQuestion.style.transition = 'transform 0.5s ease-in-out';
+      quizQuestion.style.transform = 'translateX(-1000%)';
+    });
   });
 
-  
-  const previousButton = document.querySelector('.back-section');
-  const quizQuestion = document.querySelector('.quiz_question');
-
-  previousButton.addEventListener('click', () => {
-    quizQuestion.style.transition = 'transform 0.5s ease-in-out';
-    quizQuestion.style.transform = 'translateX(-1000%)';
-  }); 
-  
-   /*
+  /*
   const nextButton = document.querySelector('next-section');
   nextButton.addEventListener('click', () => {
     // On va a la question suivante
