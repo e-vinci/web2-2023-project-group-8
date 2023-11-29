67b5c6297b751c265784f6a0d00d35bee0b274c2
Asox7 <mommensylann@gmail.com>
2023-11-29 22:06:58 +0100

    refactor: SkinCareList component and add styling

--- frontend/src/Components/Pages/SkinCareList.js
+++ frontend/src/Components/Pages/SkinCareList.js
@@ -1,10 +1,12 @@
+import { clearPage } from '../../utils/render';
+
 const main = document.querySelector('main');
 
 const Userlist = () => {
     const url = new URL(window.location.href);
     const userId = url.searchParams.get('userId');
 
-    main.innerHTML = '';
+    clearPage();
     listSkinCare(userId);
 };
 
@@ -13,7 +15,30 @@ async function listSkinCare(userId){
     const data = await response.json();
 
     data.forEach((skinCare) => {
-        main.innerHTML += `${skinCare.intitule}`;
+        const test = skinCare.expand && skinCare.expand['listes_produits(skinCare)'].map((product) => `
+            <div class="col">
+                ${product.expand.produit.nom}
+            </div>
+        `).join('');
+
+        main.innerHTML += `
+            <div class="container container-skinCare">
+                <div class="row">
+                    <div class="col-3">
+                        ${skinCare.intitule}
+                    </div>
+                    <div class="col container-product">
+                    <div class="container">
+                        <div class="row">
+                            ${test}
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+       `;
+
+       
     });
 }
 
