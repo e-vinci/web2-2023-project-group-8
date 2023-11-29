f5098680e4e4bde762896cac18c828a7f0b116e3
Asox7 <mommensylann@gmail.com>
2023-11-29 23:57:21 +0100

    feat: Add skincare list component with dynamic rendering

--- frontend/src/Components/Pages/SkinCareList.js
+++ frontend/src/Components/Pages/SkinCareList.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import { clearPage } from '../../utils/render';
 
 const main = document.querySelector('main');
@@ -11,34 +12,54 @@ const Userlist = () => {
 };
 
 async function listSkinCare(userId){
+    let accordionCounter = 1; // Variable compteur pour générer des ID uniques
+
     const response = await fetch(`http://localhost:3000/admin/skinCares?userId=${userId}`);
     const data = await response.json();
 
+    main.innerHTML += `<h1 class="text-center my-2 mb-3">Historique des skincare de XXXX</h1>`;
+
+
     data.forEach((skinCare) => {
         const test = skinCare.expand && skinCare.expand['listes_produits(skinCare)'].map((product) => `
-            <div class="col">
-                ${product.expand.produit.nom}
+            <div class="col-sm d-flex">
+                <div class="card flex-grow-1 d-flex flex-column">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.expand.produit.nom}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${product.expand.produit.contenance} ${product.expand.produit.unite_contenance}</h6>
+                        <strong>Description: </strong>
+                        <p class="card-text">
+                            ${product.expand.produit.description}
+                        </p>
+                        <h6 class="card-subtitle mb-2 text-muted">${product.expand.produit.prix} €</h6>
+                        <button type="button" class="btn btn-lg">voir le produit</button>
+                    </div>
+                </div>
             </div>
         `).join('');
 
         main.innerHTML += `
-            <div class="container container-skinCare">
-                <div class="row">
-                    <div class="col-3">
-                        ${skinCare.intitule}
-                    </div>
-                    <div class="col container-product">
-                    <div class="container">
-                        <div class="row">
-                            ${test}
+        <section class="section mx-5">
+            <div class="accordion" id="accordionPanelsStayOpenExample${++accordionCounter}">
+                <div class="accordion-item my-3">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingOne${++accordionCounter}">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne${accordionCounter}" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne${accordionCounter}">
+                            ${skinCare.intitule}
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseOne${accordionCounter}" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne${accordionCounter}">
+                        <div class="accordion-body">
+                            <div class="container">
+                                <div class="row">
+                                    ${test}
+                                </div>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
+        </section>
        `;
-
-       
     });
 }
 
