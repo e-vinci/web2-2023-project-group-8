57387ed584c47d0a3827c367feeb339845f220f0
Asox7 <mommensylann@gmail.com>
2023-12-01 23:15:34 +0100

    refactor(front): change AdminPage.js

--- frontend/src/Components/Pages/AdminPage.js
+++ frontend/src/Components/Pages/AdminPage.js
@@ -1,13 +1,5 @@
 import Navigate from '../Router/Navigate';
-import { clearPage } from '../../utils/render';
-
-const formatFrenchDate = (dateString) => {
-    const options = { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric' };
-    const date = new Date(dateString);
-    const formattedDate = date.toLocaleDateString('fr-FR', options);
-
-    return `${formattedDate}`;
-};
+import { clearPage,formatDate } from '../../utils/render';
 
 const main = document.querySelector('main');
 
@@ -22,56 +14,49 @@ async function listAllUser() {
 
     main.innerHTML  += `
     <h1 class="text-center my-2 mb-5">Liste des utilisateurs</h1>
-    <div class="content">
-        <div class="container">
-            <div class="table-responsive custom-table-responsive">
-                <table class="custom-table">
-                    <thead>
-                        <tr>
-                            <th scope="col">photo de profil</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Nom</th>
-                            <th scope="col">Prenom</th>
-                            <th scope="col">email</th>
-                            <th scope="col">Derniere connexion</th>
-                            <th scope="col">Accès</th>
-                        </tr>
-                    </thead>
-                    <tbody id="table-body">
-                    </tbody>
-                </table>
+    <div class="custom-table-responsive">
+        <div class="list-group">
+            <div class="list-group-item list-group-item-primary">
+                <div class="row align-items-start">
+                    <div class="col"></div>
+                    <div class="col">Username</div>
+                    <div class="col">Nom</div>
+                    <div class="col">Prenom</div>
+                    <div class="col-2">Email</div>
+                    <div class="col">Derniere connexion</div>
+                    <div class="col">Accès</div>
+                    <div class="col"></div>
+                </div>
             </div>
-        </div>
-    </div>`;
+        </div>  
+    </div>
+    `;
 
-    const tbody = document.querySelector('#table-body');
+    const contenu = document.querySelector('.list-group');
 
     data.forEach((user) => {
-        tbody.innerHTML += `
-        <tr class="table-response test">
-            <td class="td-spacing" style="text-align: center;">
-                <img src="${user.profileUrl}" alt="user" class="avatar">
-            </td>
-            <td class="td-spacing">@${user.username}</td>
-            <td class="td-spacing">${user.nom}</td>
-            <td class="td-spacing">${user.prenom}</td>
-            <td class="td-spacing">
-                ${user.email}
-            </td>
-            <td class="td-spacing">${formatFrenchDate(user.derniere_connexion)}</td>
-            <td class="td-spacing">${user.verified ? 'Administrateur' : 'Utilisateur'}</td>
-            <td class="td-spacing">
-                <div class="dropdown">
+        contenu.innerHTML += `
+        <ul class="list-group-item list-group-item-action" aria-current="true">
+            <li class="row align-items-start">
+                <div class="col text-center avatar">
+                    <img src="${user.profileUrl}" alt="user" class="avatar">
+                </div>
+                <div class="col">@${user.username}</div>
+                <div class="col">${user.nom}</div>
+                <div class="col">${user.prenom}</div>
+                <div class="col-2">${user.email}</div>
+                <div class="col">${formatDate(user.derniere_connexion)}</div>
+                <div class="col">${user.verified ? 'Administrateur' : 'Utilisateur'}</div>
+                <div class="col dropdown">
                     <button class="btn btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Voir Plus
                     </button>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" id="btn-skincare" data-user-id="${user.id}">Skincares</a></li>
+                        <li><a class="dropdown-item" id="btn-skincare" data-user-id="${user.id}">Liste Skincares</a></li>
                     </ul>
                 </div>
-            </td>
-            
-        </tr>
+            </li>
+        </ul>
         `;
     });
 
