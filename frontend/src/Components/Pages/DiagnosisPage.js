8c553ba5af6e06d1e28d9d3e45668d583b2b866f
Asox7 <mommensylann@gmail.com>
2023-12-14 15:21:46 +0100

    feat: Add DiagnosisPage route

---
+++ frontend/src/Components/Pages/DiagnosisPage.js
@@ -0,0 +1,52 @@
+// import { clearPage } from '../../utils/render';
+import Navigate from '../Router/Navigate';
+
+const main = document.querySelector('main');
+
+const ResultsPage = () => {
+    if (localStorage.getItem('connected') === 'true') {
+        Navigate('/results');
+    }else{
+        diagnosis();
+    }
+};
+
+function diagnosis() {
+    main.innerHTML = `<h1>Voici votre diagnostique</h1>`;
+    // Build HTML content for all products
+    const productsHTML = JSON.parse(localStorage.getItem('savdProducts')).map((product) => `
+        <div class="card mb-3">
+            <div class="row no-gutters">
+                <div class="col-md-3 d-flex align-items-center">
+                    <div class="container-fluid text-center">
+                        <img src="${product.photo}" class="card-img py-1 px-1 product-image" alt="${product.nom}">
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.nom}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${product.contenance} ${product.unite_contenance}</h6>
+                        <strong>Description: </strong>
+                        <div class="card-text">
+                            ${product.description}
+                        </div>
+                        <h6 class="card-subtitle mb-2 text-muted">${product.prix} €</h6>
+                        <button type="button" class="btn btn-lg mt-3 btn-product">voir le produit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `).join('');
+
+    // Update main.innerHTML with the entire HTML content
+    main.innerHTML += `
+        <section class="custom-table-responsive my-3 px-4 ">
+            <div class="row">
+                ${productsHTML}
+            </div>
+        </section>
+    `;
+
+};
+
+export default ResultsPage;
