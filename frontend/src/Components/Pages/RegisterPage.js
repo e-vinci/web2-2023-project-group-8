d8a5f5820631d8baf2ccc1b078da170c0d25f070
Asox7 <mommensylann@gmail.com>
2023-12-16 00:16:48 +0100

    style: refactor login and register page

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -1,75 +1,53 @@
 const main = document.querySelector('main');
 
-const LoginPage = () => {
-    main.innerHTML = `
-    <section class="h-100 gradient-form" style="background-color: #eee;">
-    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="card" style="border-radius: 15px;">
-            <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Créer un compte</h2>
-
+const RegisterPage = () => {
+    main.innerHTML += `
+      <section class="gradient-form">
+        <div class="container h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-9 col-lg-7 col-xl-6 mt-4">
+              <h2 class="text-center mb-3">Créer un compte</h2>
               <form>
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example1cg">Nom</label>
+                <div class="form-outline mb-3">
+                  <input placeholder="Nom" type="text" class="form-control form-control-lg"/>
                 </div>
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example1cg">Prénom</label>
+                <div class="form-outline mb-3">
+                  <input placeholder="Prenom" type="text" class="form-control form-control-lg" />
                 </div>
-
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example1cg">Nom d'utilisateur</label>
+                <div class="form-outline mb-3">
+                  <input placeholder="Nom d'utilisateur" type="text" class="form-control form-control-lg" />
                 </div>
-
-                <div class="form-outline mb-4">
-                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example3cg">Adresse mail</label>
+                <div class="form-outline mb-3">
+                  <input placeholder="Adresse mail" type="email" class="form-control form-control-lg" />
                 </div>
-
-                <input class="form-control form-control-lg" id="formFileLg" type="file" />
-                <div class="small text-muted mt-2">Choisissez une photo de profil. Ce champ est optionnel.</div>
-
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example4cg">Mot de passe</label>
+                <div class="form-outline mb-3">
+                  <div class="small text-muted">Choisissez une photo de profil. Ce champ est optionnel.</div>
+                  <input class="form-control form-control-lg" id="formFileLg" type="file" />
                 </div>
-
-                <div class="form-outline mb-4">
-                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example4cdg">Confirmer votre mot de passe</label>
+                <div class="form-outline mb-3">
+                  <input placeholder="Mot de passe" type="password" class="form-control form-control-lg" />
                 </div>
-
-                <div class="form-check d-flex justify-content-center mb-5">
-                  <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
+                <div class="form-outline mb-3">
+                  <input placeholder="Confirmer Mot de passe" type="password" class="form-control form-control-lg" />
+                </div>
+                <div class="form-check d-flex justify-content-center mb-4">
+                  <input class="form-check-input me-2" type="checkbox" value=""/>
                   <label class="form-check-label" for="form2Example3g">
-                    J'accepte les <a href="/t-and-c" data-uri="/t-and-c" class="text-body"><u>conditions d'utilisation</u></a>
+                    J'accepte les <a href="#!" class="text-body"><u>conditions d'utilisation</u></a>
                   </label>
                 </div>
-
                 <div class="d-flex justify-content-center">
                   <button type="button"
                     class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">S'inscrire</button>
                 </div>
-
-                <p class="text-center text-muted mt-5 mb-0">Vous avez déjà un compte ? <a href="/login"
+                <p class="text-center text-muted mt-1 mb-0">Vous avez déjà un compte ? <a href="/login"
                     class="fw-bold text-body"><u>Connectez-vous ici</u></a></p>
               </form>
-
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
-`;
+    </section>
+  `;
 }
 
-export default LoginPage;
+export default RegisterPage;
