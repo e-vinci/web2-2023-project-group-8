24d4644a90c1010db06d35d2cb694a4e91fa6e93
elisenium <elise.grelaud@student.vinci.be>
2023-12-15 04:40:39 +0100

    add: T&C file

---
+++ frontend/src/Components/Pages/TermsAndConditionsPage.js
@@ -0,0 +1,42 @@
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+body.style.overflow = 'auto';
+
+// TODO style page better
+const TermsAndConditionsPage = () => {
+    main.innerHTML = `
+    <section>
+    <h1>Conditions d'utilisations de IzySkin™</h1>
+    <ul>Dernière mise à jour le 15-12-23 04:16</ul>
+    <div>
+        <p>
+        Veuillez lire attentivement les conditions d'utilisation suivantes avant d'utiliser izyskin.com.
+        Votre accès et utilisation du Service sont conditionnés à votre acceptation et conformité avec ces Conditions. Ces Conditions s'appliquent à tous les visiteurs, utilisateurs et autres personnes qui accèdent ou utilisent le Service.
+        En accédant ou en utilisant le Service, vous acceptez d'être lié par ces Conditions. Si vous êtes en désaccord avec une partie quelconque des termes, vous ne pourrez pas accéder au Service.
+        </p>
+    </div>
+    <ul><strong>Compte et sécurité</strong></ul>
+        Pour accéder à certains aspects du Service, vous devrez peut-être créer un compte. Vous êtes responsable de maintenir la confidentialité des informations de votre compte, y compris votre mot de passe, et vous êtes entièrement responsable de toute activité qui se produit sous votre compte.
+
+        Vous acceptez de notifier immédiatement IzySkin™ de toute utilisation non autorisée de votre compte ou de toute autre violation de la sécurité. IzySkin™ ne sera pas responsable des pertes résultant de l'accès non autorisé à votre compte, et vous pourriez être tenu responsable des pertes encourues par IzySkin™ ou par d'autres utilisateurs du Service en raison de l'accès non autorisé à votre compte.
+
+    <ul><strong>Utilisation du Service</strong></ul>
+        Le Service fournit un questionnaire permettant de générer des recommandations de produits de skincare personnalisée. En utilisant ce Service, vous comprenez et acceptez que les recommandations fournies sont basées sur les informations fournies dans le questionnaire et peuvent varier en fonction des informations fournies.
+
+        Vous acceptez que les informations fournies dans le questionnaire soient exactes, complètes et à jour au meilleur de votre connaissance.
+
+    <ul><strong>Propriété intellectuelle</strong></ul>
+        Le Service et son contenu, y compris mais sans s'y limiter, le texte, les images, les graphiques ou le code, sont la propriété de IzySkin™ et sont protégés par les lois sur le droit d'auteur et autres lois sur la propriété intellectuelle.
+
+        Vous ne pouvez pas reproduire, distribuer, modifier, afficher, transmettre, vendre ou exploiter de quelque manière que ce soit, en totalité ou en partie, tout contenu obtenu à travers le Service sans le consentement écrit préalable de IzySkin™.
+
+    <ul><strong>Limitation de responsabilité</strong></ul>
+        Le Service est fourni "tel quel" sans garantie d'aucune sorte, expresse ou implicite. IzySkin™ ne garantit pas l'exactitude, la pertinence ou l'exhaustivité des recommandations de produits.
+
+        En aucun cas, IzySkin™ ne sera responsable de tout dommage direct, indirect, accidentel, spécial, consécutif ou exemplaire résultant de votre utilisation du Service ou de tout contenu obtenu à travers celui-ci.
+    </section>
+    `;
+
+}
+
+export default TermsAndConditionsPage;
